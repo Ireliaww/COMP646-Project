97b5294fba636ddb878735b2a4090bb2b226a28e
Eric Wang <cw206@rice.edu>
2025-04-29 19:37:59 -0500

    project structure update

--- Data/dataset_download.py
+++ Data/dataset_download.py
@@ -1,5 +1,11 @@
-import requests, json, os, math, sys
+import requests, json, os, math, sys, random
 
+# --------------------------
+# Configuration
+# --------------------------
+# Total number of images desired
+TOTAL_IMAGES = 1000
+# List of WikiArt styles (genre identifiers)
 styles = ["impressionismus",
           "realismus",
           "romantik",
@@ -158,83 +164,87 @@ styles = ["impressionismus",
           "spektralismus",
           "renaissance",
           "sky-art"]
+# Compute per-style quota
+num_styles = len(styles)
+per_style = math.ceil(TOTAL_IMAGES / num_styles)
 
+# Root folder where images will be saved
+dst_root = os.path.expanduser("~/wikiartData/")
+os.makedirs(dst_root, exist_ok=True)
 
-def print_progress(a):
-    sys.stdout.write('\r')
-    sys.stdout.write(a)
+# Progress helper
+def print_progress(msg):
+    sys.stdout.write(f"\r{msg}")
     sys.stdout.flush()
 
+# --------------------------
+# Functions to fetch metadata
+# --------------------------
 
-# gets the data for one page and style
 def getData(style, page):
-    url = 'https://www.wikiart.org/en/paintings-by-style/' + style
-    data = {'json': 2, 'page': page}
-    res = requests.post(url, data)
-
+    """
+    Request JSON metadata for a particular style and page.
+    """
+    url = f'https://www.wikiart.org/en/paintings-by-style/{style}'
+    payload = {'json': 2, 'page': page}
+    res = requests.post(url, payload)
     while res.status_code != 200:
-        res = requests.post(url, data)
-    out = json.loads(res.text)
-
-    return out
+        res = requests.post(url, payload)
+    return res.json()
 
 
-# gets all data for one style
 def getAllPaintings(style):
-    print("\ngettings {} paintings".format(style))
+    """
+    Retrieve all painting metadata for a given style.
+    Returns a list of metadata dicts.
+    """
     init = getData(style, 1)
-    numPaintings = init['AllPaintingsCount']
-    pages = math.ceil(numPaintings / init['PageSize']) + 1
-
+    total_count = init['AllPaintingsCount']
+    page_size = init['PageSize']
+    pages = math.ceil(total_count / page_size)
     paintings = init['Paintings']
-    for i in range(2, pages):
-        if i % 40:
-            print_progress("{}%".format(round((i / pages) * 100, 2)))
-        pageData = getData(style, i)['Paintings']
-        while (len(pageData) < 60) and (i < pages - 1):
-            print("redo" + str(i))
-            pageData = getData(style, i)['Paintings']
-        paintings = paintings + pageData
-    if not (numPaintings == len(paintings)):
-        raise Exception("More pictures expected")
+    for page in range(2, pages + 1):
+        page_data = getData(style, page)['Paintings']
+        # retry if data seems incomplete
+        while len(page_data) < page_size and page < pages:
+            page_data = getData(style, page)['Paintings']
+        paintings.extend(page_data)
     return paintings
 
+# --------------------------
+# Sampling and Download
+# --------------------------
 
-rootFolder = os.path.expanduser("~/wikiartData/")
-stylesToDownload = styles[:1]
+download_count = 0
+for style in styles:
+    print(f"Processing style '{style}' ({download_count}/{TOTAL_IMAGES} downloaded)")
+    # Fetch metadata once per style
+    all_meta = getAllPaintings(style)
+    # Randomly sample up to per_style items
+    random.shuffle(all_meta)
+    subset = all_meta[:per_style]
 
-# multiple connections would speed this up, puts more stress on the server though
-data = {}
-for style in stylesToDownload:
-    data[style] = getAllPaintings(style)
+    # Create folder for this style
+    style_dir = os.path.join(dst_root, style)
+    os.makedirs(style_dir, exist_ok=True)
 
-"""note that some pictures are inproperly named and have the jpg extention. 
-This will cause problems on programms that rely on the extention. It is possible to check with import imghdr
-imghdr.what() and rename the file accordingly.
-
-This method is also not efficient at all, best would be to have multiple threads that connect 
-and get the data, then write the files in another thread"""
-
-for style in data:
-    print("\nprocessing style '{}'".format(style))
-    folder = rootFolder + style + '/'
-
-    try:
-        os.makedirs(folder)
-    except FileExistsError:
-        print("writing to existing folder")
-
-    for i, metaData in enumerate(data[style]):
-        if i % 20 == 0:
-            print_progress("{}%".format(round(i / len(data[style]) * 100, 2)))
-
-        fullPath = folder + os.path.basename(metaData['image'])
-
-        # only make the request and save the file if the file does not exist
-        if not os.path.isfile(fullPath):
-            response = requests.get(metaData['image'])
+    # Download images
+    for meta in subset:
+        if download_count >= TOTAL_IMAGES:
+            break
+        img_url = meta.get('image')
+        img_name = os.path.basename(img_url)
+        dst_path = os.path.join(style_dir, img_name)
+        if not os.path.isfile(dst_path):
+            response = requests.get(img_url)
             while response.status_code != 200:
-                response = requests.get(metaData['image'])
-
-            with open(fullPath, 'wb') as f:
+                response = requests.get(img_url)
+            with open(dst_path, 'wb') as f:
                 f.write(response.content)
+            download_count += 1
+            print_progress(f"Downloaded {download_count}/{TOTAL_IMAGES}")
+
+    if download_count >= TOTAL_IMAGES:
+        break
+
+print("\nDone! Total images downloaded:", download_count)
